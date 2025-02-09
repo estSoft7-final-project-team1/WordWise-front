5f53f0b4f38c4077bb7d79a8b536f274d5736287
wgrgwg <kyoeng14697@naver.com>
2025-02-09 18:05:25 +0900

    feat: 전체 ui 및 검색 기능 구현

--- src/components/WordBook.js
+++ src/components/WordBook.js
@@ -20,10 +20,14 @@ const WordBook = () => {
     hasPrevious: false
   });
   const [isEmpty, setEmpty] = useState(false);
-
-  useEffect(() => {
-    fetchWordBooks(0); // 첫 페이지 데이터 불러오기
-  }, []);
+  const [searchWordText, setSearchWordText] = useState('');
+  const handleInputChange = (e) => {
+    setSearchWordText(e.target.value);
+  };
+  const startPage = Math.max(0,
+      wordBookPage.number - Math.floor(MAX_PAGE_DISPLAY / 2));
+  const endPage = Math.min(wordBookPage.totalPages - 1,
+      startPage + MAX_PAGE_DISPLAY - 1);
 
   const fetchWordBooks = async (page) => {
     try {
@@ -42,105 +46,165 @@ const WordBook = () => {
     navigate(`/api/wordbook/${wordBookId}`);
   };
 
-  const startPage = Math.max(0,
-      wordBookPage.number - Math.floor(MAX_PAGE_DISPLAY / 2));
-  const endPage = Math.min(wordBookPage.totalPages - 1,
-      startPage + MAX_PAGE_DISPLAY - 1);
+  const handleGoWordPage = async () => {
+    navigate(`/api/word`);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchWordBook();
+  };
+
+  const fetchWordBook = async () => {
+    try {
+      const response = await axios.get(`/wordbook/search/${searchWordText}`)
+      await handleWordbookClick(response.data.id);
+    } catch (error) {
+      alert(JSON.stringify(error));
+      if (error.response.status === 400) {
+        document.getElementById("noSuchWordModal").showModal();
+      } else {
+        alert("서버 오류! 다시시도해주세요.");
+      }
+    }
+  }
+
+  useEffect(() => {
+    fetchWordBooks(0); // 첫 페이지 데이터 불러오기
+  }, []);
 
   return (
-      <div>
-        <h1 className="text-3xl font-bold">내 단어장</h1>
-        {wordBookPage.content.length > 0 &&
-            <div className ="overflow-x-auto">
-              <table className="table w-full">
-                <thead>
-                <tr>
-                  <th>단어</th>
-                  <th>정의</th>
-                </tr>
-                </thead>
-                <tbody>
-                {wordBookPage.content.length > 0 &&
-                    wordBookPage.content.map((wordBook) => (
-                        <tr
-                            key={wordBook.id}
-                            className="hover cursor-pointer"
-                            onClick={() => handleWordbookClick(wordBook.id)}
-                        >
-                          <td>{wordBook.wordText}</td>
-                          <td>{wordBook.definition}</td>
-                        </tr>
-                    ))}
-                </tbody>
-              </table>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <div className="flex flex-col items-center space-y-6">
+          <h1 className="text-3xl font-bold">내 단어장</h1>
+          <div className="word-input-div">
+            <form onSubmit={handleSubmit}
+                  className="flex w-full max-w-md items-center space-x-2">
+              <input
+                  type="text"
+                  className="flex-1 h-full px-4 py-2 border border-gray-300 rounded-md"
+                  value={searchWordText}
+                  onChange={handleInputChange}
+                  pattern="[a-z]*"
+                  title="영어 소문자만 입력할 수 있습니다."
+                  placeholder="단어를 입력하세요"
+              />
+              <button className="btn btn-neutral h-full px-4 py-2"
+                      type="submit">
+                단어장 내 검색
+              </button>
+            </form>
+          </div>
 
+          <div className="main-content-div">
+            {wordBookPage.content.length > 0 &&
+                <div className="overflow-x-auto">
+                  <table className="table w-full">
+                    <thead className="text-center">
+                    <tr>
+                      <th>단어</th>
+                      <th>정의</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {wordBookPage.content.length > 0 &&
+                        wordBookPage.content.map((wordBook) => (
+                            <tr
+                                key={wordBook.id}
+                                className="hover cursor-pointer"
+                                onClick={() => handleWordbookClick(wordBook.id)}
+                            >
+                              <td className="text-center">{wordBook.wordText}</td>
+                              <td>{wordBook.definition}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                  </table>
+                </div>}
+
+            {isEmpty && <div> className="text-gray-500 text-center p-4">등록된 단어가
+              아직
+              없습니다!
             </div>}
 
-        {isEmpty && <div> className="text-gray-500 text-center p-4">등록된 단어가 아직
-          없습니다!
-        </div>}
+            {isEmpty && <div className="mt-4">
+              <button className="btn btn-neutral"
+                      onClick={() => navigate("/api/word")}>
+                단어 검색으로 이동
+              </button>
+            </div>}
+          </div>
 
-        {isEmpty && <div className="mt-4">
-          <button className="btn btn-neutral"
-                  onClick={() => navigate("/api/word")}>
-            단어 검색으로 이동
-          </button>
-        </div>}
+          {/* 페이지네이션 UI */}
+          <div className="join">
+            <button
+                className="join-item btn btn-md"
+                onClick={() => fetchWordBooks(wordBookPage.number - 1)}
+                disabled={wordBookPage.first}
+            >
+              <ChevronLeft/>
+            </button>
 
-        {/* 페이지네이션 UI */}
-        <div className="join mt-4">
-          <button
-              className="join-item btn btn-md"
-              onClick={() => fetchWordBooks(wordBookPage.number - 1)}
-              disabled={wordBookPage.first}
-          >
-            <ChevronLeft/>
-          </button>
+            {startPage > 0 && (
+                <>
+                  <button className="join-item btn btn-md"
+                          onClick={() => fetchWordBooks(0)}>1
+                  </button>
+                  {startPage > 1 && <span
+                      className="join-item btn btn-md btn-disabled">...</span>}
+                </>
+            )}
 
-          {startPage > 0 && (
-              <>
-                <button className="join-item btn btn-md"
-                        onClick={() => fetchWordBooks(0)}>1
-                </button>
-                {startPage > 1 && <span
-                    className="join-item btn btn-md btn-disabled">...</span>}
-              </>
-          )}
+            {Array.from({length: endPage - startPage + 1}, (_, index) => {
+              const pageNum = startPage + index;
+              return (
+                  <button
+                      key={pageNum}
+                      className={`join-item btn btn-md ${wordBookPage.number
+                      === pageNum ? "btn-active" : ""}`}
+                      onClick={() => fetchWordBooks(pageNum)}
+                  >
+                    {pageNum + 1}
+                  </button>
+              );
+            })}
 
-          {Array.from({length: endPage - startPage + 1}, (_, index) => {
-            const pageNum = startPage + index;
-            return (
-                <button
-                    key={pageNum}
-                    className={`join-item btn btn-md ${wordBookPage.number
-                    === pageNum ? "btn-active" : ""}`}
-                    onClick={() => fetchWordBooks(pageNum)}
-                >
-                  {pageNum + 1}
-                </button>
-            );
-          })}
+            {endPage < wordBookPage.totalPages - 1 && (
+                <>
+                  {endPage < wordBookPage.totalPages - 2 && <span
+                      className="join-item btn btn-md btn-disabled">...</span>}
+                  <button className="join-item btn btn-md"
+                          onClick={() => fetchWordBooks(
+                              wordBookPage.totalPages - 1)}>
+                    {wordBookPage.totalPages}
+                  </button>
+                </>
+            )}
 
-          {endPage < wordBookPage.totalPages - 1 && (
-              <>
-                {endPage < wordBookPage.totalPages - 2 && <span
-                    className="join-item btn btn-md btn-disabled">...</span>}
-                <button className="join-item btn btn-md"
-                        onClick={() => fetchWordBooks(
-                            wordBookPage.totalPages - 1)}>
-                  {wordBookPage.totalPages}
-                </button>
-              </>
-          )}
-
-          <button
-              className="join-item btn btn-md"
-              onClick={() => fetchWordBooks(wordBookPage.number + 1)}
-              disabled={wordBookPage.last}
-          >
-            <ChevronRight/>
-          </button>
+            <button
+                className="join-item btn btn-md"
+                onClick={() => fetchWordBooks(wordBookPage.number + 1)}
+                disabled={wordBookPage.last}
+            >
+              <ChevronRight/>
+            </button>
+          </div>
         </div>
+
+        <dialog id="noSuchWordModal" className="modal">
+          <div className="modal-box flex flex-col items-center text-center">
+              <p>해당 단어({searchWordText})가 단어장에 존재하지 않습니다!</p>
+              <p>단어 검색 페이지로 이동하시겠습니까?</p>
+            <div className="flex justify-end space-x-2 mt-4">
+              <button className="btn btn-neutral" onClick={handleGoWordPage}>단어 검색 페이지로 이동
+              </button>
+              <form method="dialog">
+                <button className="btn btn-neutral">닫기</button>
+              </form>
+            </div>
+          </div>
+        </dialog>
+
       </div>
   );
 };
--- src/components/WordMain.jsx
+++ src/components/WordMain.jsx
@@ -1,6 +1,5 @@
 import React, {useState} from "react";
 import axios from '../utils/ApiClient';
-// import "../static/css/WordMain.css";
 
 function WordMain() {
   const [wordText, setWordText] = useState("");
@@ -8,15 +7,10 @@ function WordMain() {
   const [error, setError] = useState("");
   const [checkedExamples, setCheckedExamples] = useState(new Set());
   const [wordDto, setWordDto] = useState({
-    wordText: "",
-    definition: "",
-    exampleDtos: [],
-    formerExampleIds: []
+    wordText: "", definition: "", exampleDtos: [], formerExampleIds: []
   });
   const [saveExamplesResult, setSaveResult] = useState({
-    wordBookId: null,
-    duplicatedPersonalExamples: [],
-    newPersonalExamples: []
+    wordBookId: null, duplicatedPersonalExamples: [], newPersonalExamples: []
   });
   const [isDetailPage, setIsDetailPage] = useState(false);
 
@@ -27,12 +21,12 @@ function WordMain() {
   const fetchExample = async () => {
     document.getElementById("loadingModal").showModal();
     try {
-      const response = await axios.get(
-          `/word/${wordText}`);
+      const response = await axios.get(`/word/${wordText}`);
       setWordDto(response.data);
       const examples = response.data.exampleDtos || [];
       setExampleSentences(examples);
       setError("");
+      setCheckedExamples(new Set());
     } catch (err) {
       setError("예문을 가져오는 데 실패했습니다.");
     } finally {
@@ -44,19 +38,16 @@ function WordMain() {
   };
 
   const reloadWord = async () => {
-    // check 초기화 해주기
     document.getElementById("loadingModal").showModal();
     try {
-      const response = await axios.post(
-          `/word/${wordDto.wordText}/reload`,
-          wordDto,
-          {
+      const response = await axios.post(`/word/${wordDto.wordText}/reload`,
+          wordDto, {
             headers: {"Content-Type": "application/json"},
-          }
-      );
+          });
       setWordDto(response.data);
       const examples = response.data.exampleDtos || [];
       setExampleSentences(examples);
+      setCheckedExamples(new Set());
     } catch (err) {
       alert("새로고침 실패!");
     } finally {
@@ -65,25 +56,20 @@ function WordMain() {
   };
 
   const save = async () => {
-    const selectedExamples = Array.from(checkedExamples); // 체크된 예문 리스트로 변환
+    const selectedExamples = Array.from(checkedExamples);
     if (selectedExamples.length === 0) {
-      alert("선택된 예문이 없습니다!");
+      document.getElementById("noSelectedModal").showModal();
       return;
     }
 
     const saveWordDto = {
-      ...wordDto,
-      exampleDtos: selectedExamples,
+      ...wordDto, exampleDtos: selectedExamples,
     };
 
     try {
-      const response = await axios.post(
-          `/word/save`,
-          saveWordDto,
-          {
-            headers: {"Content-Type": "application/json"},
-          }
-      );
+      const response = await axios.post(`/word/save`, saveWordDto, {
+        headers: {"Content-Type": "application/json"},
+      });
 
       setSaveResult(response.data);
       setCheckedExamples(new Set());
@@ -91,7 +77,7 @@ function WordMain() {
     } catch (error) {
       if (error.response) {
         if (error.response.status === 409) {
-          alert("이미 존재하는 단어입니다.");
+          document.getElementById("duplicateModal").showModal();
         }
       } else {
         alert("서버 오류! 다시시도해주세요.");
@@ -117,98 +103,137 @@ function WordMain() {
   };
 
   return (
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input
-              type="text"
-              className="w-full max-w-xs"
-              value={wordText}
-              onChange={handleInputChange}
-              pattern="[a-z]*"
-              title="영어 소문자만 입력할 수 있습니다."
-              placeholder="단어를 입력하세요"
-          />
-          <button className="btn btn-neutral" type="submit">예문 가져오기!</button>
-        </form>
-        <div className="word-div">
-        {error && <p style={{color: "red"}}>{error}</p>}
-        {isDetailPage && <h1 className="text-3xl font-bold">정의</h1>}
-        {wordDto.definition && <p className="mt-3">{wordDto.definition}</p>}
-        {isDetailPage && <h1 className="text-3xl font-bold mt-3">예문</h1>}
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <div className="flex flex-col items-center space-y-4">
+          <div className="word-input-div">
+            <form onSubmit={handleSubmit}
+                  className="flex w-full max-w-md items-center space-x-2">
+              <input
+                  type="text"
+                  className="flex-1 h-full px-4 py-2 border border-gray-300 rounded-md"
+                  value={wordText}
+                  onChange={handleInputChange}
+                  pattern="[a-z]*"
+                  title="영어 소문자만 입력할 수 있습니다."
+                  placeholder="단어를 입력하세요"
+              />
+              <button className="btn btn-neutral h-full px-4 py-2"
+                      type="submit">
+                예문 가져오기
+              </button>
+            </form>
+          </div>
+
+          <div className="word-content-div text-center">
+            {error && <p className="text-red-500">{error}</p>}
+            {isDetailPage && <h1 className="text-3xl font-bold">정의</h1>}
+            {wordDto.definition && <p
+                className="mt-3 text-xl">{wordDto.definition}</p>}
+            {isDetailPage && <h1 className="text-3xl font-bold mt-3">예문</h1>}
+          </div>
+
+          <ul className="mt-3">
+            {exampleSentences.map((example, index) => (
+                <li className="mt-3 flex items-center space-x-3 text-xl"
+                    key={index}>
+                  <div className="flex items-center h-full">
+                    <input
+                        type="checkbox"
+                        className="checkbox h-6 w-6"
+                        checked={checkedExamples.has(example)}
+                        onChange={() => handleCheckboxChange(example)}
+                    />
+                  </div>
+                  <div className="sentence-content flex-1">
+                    <p>{example.sentence}</p>
+                    <p>{example.sentenceMeaning}</p>
+                  </div>
+                </li>))}
+          </ul>
+
+          {isDetailPage && (<div className="button-div flex space-x-3 mt-3">
+            <div className="relative group">
+              <button
+                  className="btn btn-neutral"
+                  onClick={reloadWord}
+                  disabled={!wordText}>
+                예문 새로고침
+              </button>
+              <span
+                  className="absolute -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap rounded-md
+                      bg-gray-800 px-3 py-1 text-sm text-white opacity-0 transition-opacity
+                      group-hover:opacity-100">새로운 예문들을 불러옵니다.
+              </span>
+            </div>
+
+            <div className="relative group">
+              <button className="btn btn-neutral" onClick={save}>단어장에 저장
+              </button>
+              <span
+                  className="absolute -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap rounded-md
+                      bg-gray-800 px-3 py-1 text-sm text-white opacity-0 transition-opacity
+                      group-hover:opacity-100">선택한 예문들을 내 단어장에 저장합니다.
+              </span>
+            </div>
+          </div>)}
         </div>
-        <ul className="mt-3">
-          {exampleSentences.map((example, index) => (
-              <li className="mt-3" key={index}>
-                <input
-                    type="checkbox"
-                    className = "checkbox"
-                    checked={checkedExamples.has(example)}
-                    onChange={() => handleCheckboxChange(example)}
-                />
-                <div>
-                  <p>
-                    <strong>예문: </strong> {example.sentence}
-                  </p>
-                  <p>
-                    <strong>해석: </strong> {example.sentenceMeaning}
-                  </p>
-                </div>
-              </li>
-          ))}
-        </ul>
-        {isDetailPage && <div className="button-div mt-3">
-          <button className="btn btn-neutral" onClick={reloadWord} disabled={!wordText}>
-            예문 새로고침
-          </button>
-          <button className="btn btn-neutral ml-3" onClick={save}>
-            저장
-          </button>
-        </div>}
 
         {/* 모달 */}
         <dialog id="loadingModal" className="modal">
-          <div className="modal-box">
+          <div
+              className="modal-box flex flex-row items-center justify-center text-center w-full max-w-md space-x-2">
             <p>예문을 생성중입니다</p>
-            <span className="loading loading-spinner loading-md"></span>
+            <span className="loading loading-spinner loading-md mt-2"></span>
+          </div>
+        </dialog>
+
+        <dialog id="duplicateModal" className="modal">
+          <div className="modal-box flex flex-col items-center text-center">
+            <p className="text-red-500">내 단어장에 이미 존재하는 예문입니다!</p>
+            <form method="dialog" className="mt-3">
+              <button className="btn btn-neutral">닫기</button>
+            </form>
+          </div>
+        </dialog>
+
+        <dialog id="noSelectedModal" className="modal">
+          <div className="modal-box flex flex-col items-center text-center">
+            <p className="text-red-500">선택된 예문이 없습니다!</p>
+            <form method="dialog" className="mt-3">
+              <button className="btn btn-neutral">닫기</button>
+            </form>
           </div>
         </dialog>
 
         <dialog id="saveResultModal" className="modal">
-          <div className="modal-box">
-            <p>저장되었습니다!</p>
+          <div className="modal-box flex flex-col items-center text-center">
+            <p>내 단어장에 성공적으로 저장되었습니다!</p>
             {saveExamplesResult.newPersonalExamples.length > 0 && (
-                <>
-                  <h3>저장된 예문</h3>
-                  <ul>
+                <div className="w-full mt-5">
+                  <h3 className="text-xl text-green-500">저장된 예문</h3>
+                  <ul className="list-decimal text-left w-full space-y-2">
                     {saveExamplesResult.newPersonalExamples.map(
-                        (example, index) => (
-                            <li key={index}>
-                              <p><strong></strong> {example}</p>
-                            </li>
-                        ))}
+                        (example, index) => (<li key={index}>
+                          <p>{example}</p>
+                        </li>))}
                   </ul>
-                </>
-            )}
+                </div>)}
             {saveExamplesResult.duplicatedPersonalExamples.length > 0 && (
-                <>
-                  <h3>이미 저장된 예문</h3>
-                  <ul>
+                <div className="w-full mt-3">
+                  <h3 className="text-xl text-red-500">이미 존재하는 예문</h3>
+                  <ul className="list-decimal text-left w-full space-y-2">
                     {saveExamplesResult.duplicatedPersonalExamples.map(
-                        (example, index) => (
-                            <li key={index}>
-                              <p><strong></strong> {example}</p>
-                            </li>
-                        ))}
+                        (example, index) => (<li key={index}>
+                          <p>{example}</p>
+                        </li>))}
                   </ul>
-                </>
-            )}
-            <form method="dialog">
-              <button>닫기</button>
+                </div>)}
+            <form method="dialog" className="mt-3">
+              <button className="btn btn-neutral">닫기</button>
             </form>
           </div>
         </dialog>
-      </div>
-  );
+      </div>);
 }
 
 export default WordMain;
